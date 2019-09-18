e3fa35af50e3ec1e9ee65afa2268f5c566dcbd14
Rob Kutner <Rob.Kutner@gmail.com>
2019-09-17 18:37:17 -0700

    buttons add properly

--- giphy.js
+++ giphy.js
@@ -1,28 +1,46 @@
 // name variables
-var searchButtons = ["puppy", "kitten", "baby bunny"]
-var search = $("#addgif").val();
+var topicButtons = ["puppy", "kitten", "baby bunny"];
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ZsEgA2iFC8Yc2Ukf5mf2oa1ekLmkSvyJ&limit=10";
+var search;
+var addTopic;
 
 // show buttons
 function displayButtons() {
     $("#buttonsDiv").empty();
-    for (var i = 0; i < searchButtons.length; i++) {
+    for (var i = 0; i < topicButtons.length; i++) {
         var b = $("<button>");
         b.addClass("gif");
-        b.attr("gifTopic", searchButtons[i]);
-        b.text(searchButtons[i]);
+        b.attr("gifTopic", topicButtons[i]);
+        b.text(topicButtons[i]);
         $("#buttonsDiv").append(b);
-        console.log(searchButtons[i]);
     }
 }
-var queryURL = `https://api.giphy.com/v1/gifs/search/&q=${"#search"}&limit=1&api_key=ZsEgA2iFC8Yc2Ukf5mf2oa1ekLmkSvyJ`;
+
+// add to buttons
 $("#addGif").on("click", function (event) {
+    event.preventDefault()
+        addTopic = $("#gifsInput").val().trim();
+        topicButtons.push(addTopic)
+        displayButtons()
+
+
+    })
+
+
+
+
+$("#gif").on("click", function(event){
+    event.preventDefault()
+    search= (this).gifTopic;
+    console.log(search);
     $.ajax({
         url: queryURL,
         methos: "GET"
-    }).then(function (response){
-    console.log(response)
-    
-    
-    })
+    }).then(function (response) {
 
-})
+        console.log(response);
+
+
+
+})});
+displayButtons()
