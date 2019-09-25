eef2d1d22b6b1d6df761752b5d7577371a9944b3
Rob Kutner <Rob.Kutner@gmail.com>
2019-09-25 09:19:01 -0700

    gifs add, but horizontally

--- giphy.js
+++ giphy.js
@@ -1,7 +1,6 @@
 // name variables
-var topicButtons = ["puppy", "kitten", "baby bunny"];
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=ZsEgA2iFC8Yc2Ukf5mf2oa1ekLmkSvyJ&limit=10";
-var search;
+var topicButtons = ["puppy", "kitten", "baby_bunny"];
+
 var addTopic;
 
 // show buttons
@@ -10,37 +9,48 @@ function displayButtons() {
     for (var i = 0; i < topicButtons.length; i++) {
         var b = $("<button>");
         b.addClass("gif");
-        b.attr("gifTopic", topicButtons[i]);
+        b.attr('gifTopic', topicButtons[i]);
         b.text(topicButtons[i]);
         $("#buttonsDiv").append(b);
     }
 }
-
+displayButtons()
 // add to buttons
 $("#addGif").on("click", function (event) {
     event.preventDefault()
         addTopic = $("#gifsInput").val().trim();
-        topicButtons.push(addTopic)
+        if(!(topicButtons.includes(addTopic))){
+            topicButtons.push(addTopic)
+        }
         displayButtons()
-
-
     })
-
-
-
-
-$("#gif").on("click", function(event){
-    event.preventDefault()
-    search= (this).gifTopic;
-    console.log(search);
-    $.ajax({
-        url: queryURL,
-        methos: "GET"
-    }).then(function (response) {
-
+    
+    
+    $(".gif").on("click", function(event){
+        event.preventDefault()
+        var search= $(this).attr('gifTopic');
+        var queryURL = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=ZsEgA2iFC8Yc2Ukf5mf2oa1ekLmkSvyJ&limit=1`;
+        $.ajax({
+            url: queryURL,
+            methos: "GET"
+        }).then(function (response) {
+            
+        // console.log(response.data[0].bitly_url);
         console.log(response);
+        var crd= $("<div>");
+        crd.addClass("card")
+
+        var g = $("<img>");
+        var gp= "";
+        g.addClass("anim")
+        g.attr('src', response.data[0].images.fixed_height.url)
+        $('#gifsView').prepend(crd);
+        gp= (response.data[0].rating);
+        crd.append("Rating: "+ gp)
+        crd.append(g)
+
+
 
 
 
 })});
-displayButtons()
